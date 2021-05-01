4953e98b326edff4f87caedf5ead27646421c9c7
Suhaila Achmad <suhaila2008@gmail.com>
2021-05-01 12:58:57 +0700

    Tugas 2

--- Tugas-2/Soal-1.py
+++ Tugas-2/Soal-1.py
@@ -1,23 +1,28 @@
 Daftar_kontak = {
-    "Nama" : "Suhaila",
-    "No_Hp" : "085211041000",
-    "Nama" : "Ayla",
-    "No_Hp" : "085215256556",
+    "nama" : "Suhaila",
+    "telp" : "085211041000",
 }
 x = int(input("Pilih Menu : "))
-pelanggan_list = []
+kontak_list = []
 
 if x == 1 :
-    print("DAFTAR KONTAK")
-    print("Nama Kontak :",(Daftar_kontak["Nama"]))
-    print("No HP :",(Daftar_kontak["No_Hp"]))
+    print("===DAFTAR KONTAK===")
+    print(Daftar_kontak["nama"])
+    print(Daftar_kontak["telp"])
 elif x == 2 :
-    for i in range(1):
-    nama = input("Masukkan Nama Kontak : ")
-    telp = input("Masukkan Nomor HP : ")
-    data = {
-        "Nama" : nama,
-        "No_Hp" : telp,
-    }
-    pelanggan_list.append(data)
+    for i in range(2):
+        nama = input("Masukkan Nama Kontak : ")
+        telp = input("Masukkan Nomor HP : ")
+        data = {
+            "nama" : nama,
+            "telp" : telp,
+        }
+        kontak_list.append(data)
+    for i in kontak_list:
+        print("Nama Kontak : ",i["nama"])
+        print("No HP : ",i["telp"])
     print("Kontak berhasil ditambahkan")
+elif x == 3 :
+    print("Program selesai sampai jumpa!")
+else:
+    print("Menu tidak tersedia")
